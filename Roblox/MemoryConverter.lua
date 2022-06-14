68a950a6ca1a542246ff4db43b1154e4d5617302
kura <89491373+kuraise@users.noreply.github.com>
2022-06-14 16:01:10 -0400

    Create MemoryConverter.lua

---
+++ Roblox/MemoryConverter.lua
@@ -0,0 +1,26 @@
+--[[
+  Owner: nickk#5538
+  Name: MemoryConverter.lua
+  Description: Converts Roblox memory addresses to bits, can be useful for checking if there's a high memory usage.
+  More Desc: You just need a bit of your brain to use it correctly.
+]]
+
+-- Source
+local sizes = {"MB", "GB", "TB" }
+
+local Stats = game:GetService("Stats")
+local Memory = Stats:GetTotalMemoryUsageMb()
+
+local function convertBytes(size)
+    local i = 1
+    while size > 1024 do
+        size = size / 1024
+        i = i + 1
+    end
+    return string.format("%.2f %s", size, sizes[i])
+end
+
+-- usage
+if convertBytes(Memory) > maxInt .. "MB" then
+  -- code!!11!!111!!
+end
